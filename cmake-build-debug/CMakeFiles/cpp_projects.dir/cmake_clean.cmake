37b60ade382982fd889ff7ae594642d14818a9dc
ozzafar <ozzafar@mail.tau.ac.il>
2020-03-28 15:01:48 +0300

    test

---
+++ cmake-build-debug/CMakeFiles/cpp_projects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_projects.dir/main.cpp.o"
+  "cpp_projects"
+  "cpp_projects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
